c79b1277db316d3b2215b111d53e3323823446e6
Salim <salim7ali@gmail.com>
2024-10-15 23:33:32 +0530

    moved to folder

---
+++ rotate_matrix_clockwise_once/rotate_matrix_right_once.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+// #include <vector>
+// #include <iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+    vector<vector<int>> matrix = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    vector<vector<int>> rotatedMatrix(n, vector<int> (n, 0));
+    
+    int newR, newC;
+    for(int r=0; r<matrix.size(); r++){
+        for(int c=0; c<matrix[0].size(); c++){
+            newR = c;
+            newC = n-r-1;
+            rotatedMatrix[newR][newC] = matrix[r][c];
+        }
+    }
+
+    for(int r=0; r<rotatedMatrix.size(); r++){
+        for(int c=0; c<rotatedMatrix[0].size(); c++){
+            cout<<rotatedMatrix[r][c]<<" ";
+        }cout<<"\n";
+    }
+}
