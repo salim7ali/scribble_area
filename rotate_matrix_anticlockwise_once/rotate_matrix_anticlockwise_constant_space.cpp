27b8a6349aee921205c7a9393d0f60a8412710d0
Salim <salim7ali@gmail.com>
2024-10-16 23:22:07 +0530

    Reworked entire logic to use matrix transpose followed with reverse colum wise. Constant space complexity achieved.

--- rotate_matrix_anticlockwise_once/rotate_matrix_anticlockwise_constant_space.cpp
+++ rotate_matrix_anticlockwise_once/rotate_matrix_anticlockwise_constant_space.cpp
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=Z0R2u6gd3GU
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
@@ -5,43 +6,29 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int halfSize, MATRIX_SIZE;
-        MATRIX_SIZE = matrix.size();
-        if(matrix.size()%2 == 0){   // even length
-            halfSize = (matrix.size()/2)-1;
-        }else{                      // odd length
-            halfSize = matrix.size()/2;     // take floor
-        }
+        int MATRIX_SIZE = matrix.size();
 
-        int bufferR, bufferC, bufferValue;
-        int cornerRow, cornerCol;
-        int cornerNewRow, cornerNewCol;
-
-        // Assuming square matrix
-        for(int r=0; r<=halfSize; r++){
-            for(int c=0; c<=halfSize; c++){
-                // buffer the row, rol and value which will be overwritten due to current position's 4 corner transform
-                bufferR = MATRIX_SIZE - c - 1;
-                bufferC = r;
-                bufferValue = matrix[bufferR][bufferC]; 
-
-                cornerRow = r;
-                cornerCol = c;
-                for(int i=0; i<4; i++){     // 4 corresponding corners of matrix based on current position 
-                    cornerNewRow = MATRIX_SIZE - cornerCol - 1;
-                    cornerNewCol = cornerRow;
-
-                    matrix[cornerNewRow][cornerNewCol] = matrix[cornerRow][cornerCol];
-
-                    int tempCornerRow = cornerRow;
-                    cornerRow = cornerCol;
-                    cornerCol = MATRIX_SIZE - tempCornerRow -1; 
-                }
+        // matrix transposed 
+        for(int i=1; i<MATRIX_SIZE; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
+        int mid = (MATRIX_SIZE/2)-1;    // works both for odd length and even length
+        // reverse matrix
+        for(int i=0; i<=mid; i++){
+            for(int j=0; j<MATRIX_SIZE; j++){
+                swap(matrix[i][j], matrix[MATRIX_SIZE-i-1][j]);
+            }
+        }
+
+        this->displayMatrix(matrix);
+    }
+
+    void displayMatrix(vector<vector<int>> matrix){
         for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
+            for(int j=0; j<matrix[0].size();j++){
                 cout<<matrix[i][j]<<" ";
             }cout<<"\n";
         }
