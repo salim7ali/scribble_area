83cdef2763a4e72ab777c9369c0896a00c515f04
Salim <salim7ali@gmail.com>
2024-10-18 00:12:20 +0530

    basic logic added

--- rotate_matrix_clockwise_once/count_number_of_islands/number_of_islands.cpp
+++ rotate_matrix_clockwise_once/count_number_of_islands/number_of_islands.cpp
@@ -1,8 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(int i, int j, vector<vector<int>> &islandMatrix, int &islandCount){
+void dfs(int i, int j, vector<vector<int>> &islandMatrix){
+    if(islandMatrix[i][j] != 1) //empty or visited previously
+        return;
 
+    islandMatrix[i][j] = 2;     // marked as visited
+
+    vector<pair<int, int>> relativeNeighPositions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int currentNeighbourRow, currentNeighbourCol;
+    for(int neighIndex=0; neighIndex<4; neighIndex++){
+        currentNeighbourRow = i + relativeNeighPositions[neighIndex].first;
+        currentNeighbourCol = j + relativeNeighPositions[neighIndex].second;
+
+        if(isValidPosition(currentNeighbourRow, currentNeighbourCol) == false)
+            continue;
+        
+        dfs(currentNeighbourRow, currentNeighbourCol, islandMatrix);
+    }
+
+    
 }
 
 int main(){
@@ -18,8 +35,13 @@ int main(){
 
     for(int i=0; i<islandMatrix.size(); i++){
         for(int j=0; j<islandMatrix[0].size(); j++){
-            dfs(i, j, islandMatrix, islandCount);
+            if(islandMatrix[i][j] == 1){
+                islandCount += 1;
+                dfs(i, j, islandMatrix);
+            }
         }
     }
+
+    cout<<islandCount;
     
 }
