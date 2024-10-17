71cf8e4f1b18c63a38e9b4c6b9937b546978a326
Salim <salim7ali@gmail.com>
2024-10-17 23:50:03 +0530

    main for loop set

--- rotate_matrix_clockwise_once/count_number_of_islands/number_of_islands.cpp
+++ rotate_matrix_clockwise_once/count_number_of_islands/number_of_islands.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(int i, int j, vector<vector<int>> &islandMatrix, int &islandCount){
+
+}
+
 int main(){
+    vector<vector<int>> islandMatrix = {
+        {0, 1, 1, 0, 1},
+        {0, 1, 0, 1, 0},
+        {0, 0, 1, 0, 1},
+        {0, 1, 0, 1, 1},
+        {0, 1, 0, 1, 0},
+    };
+
+    int islandCount=0;
+
+    for(int i=0; i<islandMatrix.size(); i++){
+        for(int j=0; j<islandMatrix[0].size(); j++){
+            dfs(i, j, islandMatrix, islandCount);
+        }
+    }
     
 }
