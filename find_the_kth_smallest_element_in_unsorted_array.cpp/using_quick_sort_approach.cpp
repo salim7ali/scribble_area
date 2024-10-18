6a2e2dedcf3f096377e3b08da278d12dba8c2f90
Salim <salim7ali@gmail.com>
2024-10-19 00:25:21 +0530

    logic added

--- find_the_kth_smallest_element_in_unsorted_array.cpp/using_quick_sort_approach.cpp
+++ find_the_kth_smallest_element_in_unsorted_array.cpp/using_quick_sort_approach.cpp
@@ -2,13 +2,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class kthElement{
 
-};
+int quickSort(int arr[], int l, int r, int k){
+    int p = l-1;
+    int runner = p+1;
+
+    while(runner<r){
+        if(arr[runner] < arr[r]){
+            p++;
+            swap(arr[p], arr[runner]);
+        }else{
+            runner++;
+        }
+    }
+
+    swap(arr[p+1], arr[r]);
+    int currentFixedNumber = p+2;
+
+    if(k==currentFixedNumber)
+        return arr[p+1];
+    else if(k<currentFixedNumber){
+        quickSort(arr, l, p, k);
+    }else{
+        quickSort(arr, p+2, r, k);
+    }
+}
 
 int main(){
     int array[] = {7, 10, 4, 3, 20, 15};
+    int arrSize = sizeof(array)/sizeof(array[0]);
+
+    int k = 2;
+    cout<< quickSort(array, 0, arrSize-1, k);
     
-    kthElement obj = new kthElement;
+    
      
 }
