f17a227338f530d94fabc722e753da6c97411864
Salim <salim7ali@gmail.com>
2024-10-18 22:44:38 +0530

    boiler plate

---
+++ find_the_kth_smallest_element_in_unsorted_array.cpp/using_quick_sort_approach.cpp
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/kth-smallest-largest-element-in-unsorted-array/
+#include<bits/stdc++.h>
+using namespace std;
+
+class kthElement{
+
+};
+
+int main(){
+    int array[] = {7, 10, 4, 3, 20, 15};
+    
+    kthElement obj = new kthElement;
+     
+}
